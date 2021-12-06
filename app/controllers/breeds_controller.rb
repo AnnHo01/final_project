a4d8e93a415d6f68a4ee59ff71dd712d64d995e7
AnnHo01 <punri1304@gmail.com>
2021-12-06 05:25:48 -0600

    commit before pulling merged request

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -14,13 +14,13 @@ class BreedsController < ApplicationController
     puts breed_search_2
     if(breed_search_2.empty?)
       @breeds = Breed.where("name LIKE ?", wildcard_search).page(params[:page])
-      if @breeds
+      if @breeds.empty?
         @breeds = Breed.where("description LIKE ?", wildcard_search).page(params[:page])
       end
     else
       @breeds = Breed.where("lower(name) LIKE ? AND pet_group_id = ?", wildcard_search, breed_search_2).page(params[:page])
 
-      if @breeds
+      if @breeds.empty?
         @breeds = Breed.where("lower(description) LIKE ? AND pet_group_id = ?", wildcard_search, breed_search_2).page(params[:page])
       end
     end
